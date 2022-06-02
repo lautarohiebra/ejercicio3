6657ddaff3f99fc03063722429696511e1ad6a4c
Lautaro Hiebra <lautarohiebra@gmail.com>
2022-06-01 23:06:15 -0300

    Ejercicio terminado

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('Lautaro Hiebra, Martin Maidana, BC:592066 ')
+
+let nombre = prompt('Ingrese su nombre completo')
+
+let edad = prompt('Ingrese su edad')
+
+if (edad <= 30) {
+    alert('Hola, ¿como andas?')
+} else {
+    alert(`Bienvenido ${nombre} ¿Como estas?`)
+}
